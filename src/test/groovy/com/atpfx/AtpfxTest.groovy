c7061b65455df204ff76e3b8bcf8df105805dc30
Cosmin Juravle <cosmin.juravle@nespresso.com>
2015-03-25 08:09:29 +0100

    Added base class for integration tests, profile "test" uses now h2 as embedded db for testing.

---
+++ src/test/groovy/com/atpfx/AtpfxTest.groovy
@@ -0,0 +1,8 @@
+package com.atpfx
+
+class AtpfxTest extends BaseIntegrationTest {
+
+    def "context loads correctly for Atpfx app"() {
+        expect: 1
+    }
+}
---
+++ src/test/groovy/com/atpfx/BaseIntegrationTest.groovy
@@ -0,0 +1,17 @@
+package com.atpfx
+
+import org.springframework.boot.test.IntegrationTest
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.transaction.TransactionConfiguration
+import org.springframework.transaction.annotation.Transactional
+import spock.lang.Specification
+
+@ActiveProfiles("test")
+@IntegrationTest("server.port:0")
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
+@ContextConfiguration(loader = SpringApplicationContextLoader, classes = Atpfx)
+abstract class BaseIntegrationTest extends Specification {
+}
